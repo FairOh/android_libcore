63489f7c963588ff1daf94f636d333360791cdf8
Elliott Hughes <enh@google.com>
2013-09-04 15:38:20 -0700

    Only use public API for icu4c's FieldPositionIterator.
    Change-Id: I247f7c644537b8e9f67f4f27360ce1449fa04300
    
    Conflicts:
    	luni/src/main/native/IcuUtilities.h

--- luni/src/main/native/IcuUtilities.h
+++ luni/src/main/native/IcuUtilities.h
@@ -17,6 +17,9 @@
 #ifndef ICU_UTILITIES_H_included
 #define ICU_UTILITIES_H_included
 
+#undef U_HAVE_STD_STRING
+#define U_HAVE_STD_STRING 1 // For UnicodeString::toUTF8String(std::string&).
+
 #include "jni.h"
 #include "unicode/utypes.h" // For UErrorCode.
 #include "unicode/locid.h" // For Locale.
